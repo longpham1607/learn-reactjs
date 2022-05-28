38faacaeeb43ad64d8a75c333bca8f5f3df6dbea
longpham1607 <longpham16072001@gmail.com>
2022-05-28 19:52:35 +0700

    add jsconfig.json

--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import InputField from "../../../../components/form-controls/InputField";
+
 import { useForm } from "react-hook-form";
+import InputField from "components/form-controls/InputField";
 
 function TodoForm(props) {
   const form = useForm({
